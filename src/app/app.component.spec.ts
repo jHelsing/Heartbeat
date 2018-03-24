b9e4c7270209858dfcbcc4601500fa44db9d70d3
Helsing <jonathan.helsing@gmail.com>
2018-03-24 13:27:13 +0100

    Restored united test
    It will not be true as before!

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 // @TODO Replace this test with something useful
 describe('An example test', () => {
   it('should do nothing', () => {
-    expect(false).toBeTruthy();
+    expect(true).toBeTruthy();
   });
 });
