b2a364093180c6f04fc3e398adff8aa536ca8f18
Christoffer Karlsson <chriskar@student.chalmers.se>
2018-04-09 22:19:55 +0200

    Add Firebase configuration file

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkdxRl5CL5gaB26OS97ZXTq2neZXAPNok',
+  authDomain: 'heartbeat-9f255.firebaseapp.com',
+  databaseURL: 'https://heartbeat-9f255.firebaseio.com',
+  projectId: 'heartbeat-9f255',
+  storageBucket: 'heartbeat-9f255.appspot.com',
+  messagingSenderId: '65893197427',
+};
+
+export default firebaseConfig;
