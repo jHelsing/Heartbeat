ddbc54f0adc4fb3951264724e2a1f2a847c3944f
Jonathan Helsing <Jonathan@Jonathans-MacBook-Pro.local>
2018-04-19 13:45:36 +0200

    Cleaned file and clarified comment

--- src/providers/storage/storage.ts
+++ src/providers/storage/storage.ts
@@ -5,13 +5,13 @@ import Blob = firebase.firestore.Blob;
 @Injectable()
 export class Storage {
 
-  public uploadImage(image: Blob, fileName: string){
+  public uploadImage(image: Blob, fileName: string) {
     const storageRef = firebase.storage().ref(fileName);
     console.log(storageRef.put(image).snapshot.downloadURL);
   }
   /*
    * The function is called as follows where the string in storage.getUrl is the name of the file. The return is a
-   * promise that directly contains the url inside then.
+   * promise that directly contains the url inside then. This url can then be assigned to a img tag as the source.
    * const storage = new Storage();
    * const result = storage.getUrl('Screen Shot 2018-04-19 at 10.47.01.png');
    * result.then((url) => {
