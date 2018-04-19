6e712425dde1e7ebad00bf80d611f29d10e90350
leigh <lagebergman@gmail.com>
2018-04-19 15:13:29 +0200

    Change from using firebase to AngularFirestore

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import firebase from 'firebase';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { LoginProvider } from '../../providers/login/login';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @IonicPage()
 @Component({
@@ -10,25 +10,27 @@ import { LoginProvider } from '../../providers/login/login';
 })
  export class LoginPage {
 
-  public static roleCollectionNames = ['nurses', 'doctors'];
-
   // Used to read the input fields from HTML elements with [(ngModel)].
-  private inputEmail: string;
-  private inputPassword: string;
+  private inputEmail: string = '';
+  private inputPassword: string = '';
+  private roleCollectionNames = ['nurses', 'doctors'];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loginProvider: LoginProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public loginProvider: LoginProvider, public fireStore: AngularFirestore,
+              public toastController: ToastController) {
 
     // Check if the current device is already logged in as a user. If so, continue that session.
     this.loginProvider.checkLoggedIn(this.loadCorrectPage);
   }
 
-  public loadCorrectPage(userID) {
-    const db = firebase.firestore();
-    for (const roleCollectionName of LoginPage.roleCollectionNames) {
+  public loadCorrectPage = (userID) => {
+    for (const roleCollectionName of this.roleCollectionNames) {
 
       // Look for userID in collection. If it exists in category, user has role.
-      db.collection(roleCollectionName).doc(userID).get().then((doc) => {
-        if (doc.exists) {
+      const userPromise = this.fireStore.collection<any>('/' + roleCollectionName).doc(userID).ref.get();
+
+      userPromise.then((user) => {
+        if (user.exists) {
           alert('User is in role category = ' + roleCollectionName);
           // TODO: Load correct page.
         }
@@ -38,8 +40,19 @@ import { LoginProvider } from '../../providers/login/login';
 
   // Fetch username and password from input fields. Log user in.
   public login() {
-    this.loginProvider.login(this.inputEmail, this.inputPassword).then((promise) => {
-      this.loadCorrectPage(promise.uid);
+    this.loginProvider.login(this.inputEmail, this.inputPassword)
+    .catch((e) => {
+      const toast = this.toastController.create({
+        message: e.message,
+        duration: 3000,
+        position: 'bot',
+      });
+      toast.present();
+    })
+    .then((promise) => {
+      if (promise) {
+        this.loadCorrectPage(promise.uid);
+      }
     });
   }
 
