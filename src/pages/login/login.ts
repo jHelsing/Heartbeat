a1c66714c6ea960e9f0f78bbd9fd79870f4b6080
leigh <lagebergman@gmail.com>
2018-04-19 13:01:51 +0200

    Loop over list of roles when checking role of logged in user

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -13,13 +13,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
   // Used to read the input fields from HTML elements with [(ngModel)].
   private inputEmail;
   private inputPassword;
+  private roleCollectionNames = ['nurses', 'doctors'];
 
   constructor(public afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
 
     // Listen for auth state once when app starts to continue an ongoing session, then unsubscribe.
     const authObserver = afAuth.authState.subscribe((user) => {
       if (user) {
-        this.checkRole(user.uid); // User already logged in, check role and load corresponding page.
+        this.loadCorrectPage(user.uid); // User already logged in, check role and load corresponding page.
         authObserver.unsubscribe();
       } else {
         // Nog logged in
@@ -28,35 +29,33 @@ import { AngularFireAuth } from 'angularfire2/auth';
     });
   }
 
-  public checkRole(userID) {
-    // Load page corresponding to user role.
-    this.loadCorrectPage(userID, 'nurses');
-    this.loadCorrectPage(userID, 'doctors');
+  public loadCorrectPage(userID) {
+    const db = firebase.firestore();
+    for (const roleCollectionName of this.roleCollectionNames) {
+
+      // Look for userID in collection. If it exists in category, user has role.
+      db.collection(roleCollectionName).doc(userID).get().then((doc) => {
+
+        if (doc.exists) {
+          alert('User is in role category = ' + roleCollectionName);
+          // TODO: Load correct page.
+        }
+
+      });
+    }
   }
 
   // Fetch username and password from input fields. Log user in.
   public login() {
-    this.afAuth.auth.signInWithEmailAndPassword(this.inputEmail, this.inputPassword);
+    this.afAuth.auth.signInWithEmailAndPassword(this.inputEmail, this.inputPassword).then((promise) => {
+      this.loadCorrectPage(promise.uid);
+    });
 
   }
 
   // Log user out.
   public logout() {
     this.afAuth.auth.signOut();
-  }
-
-  private loadCorrectPage(userID, roleCollectionName) {
-    const db = firebase.firestore();
-
-    // Look for userID in collection. If it exists in category, user has role.
-    db.collection(roleCollectionName).doc(userID).get().then((doc) => {
-
-      if (doc.exists) {
-        alert('User is in role category = ' + roleCollectionName);
-        // TODO: Load correct page.
-      }
-
-    });
   }
 
 }
