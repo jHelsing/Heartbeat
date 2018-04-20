12f198158326d472b671c81e7d45f2709e64e871
Jonathan Helsing <Jonathan@Jonathans-MacBook-Pro.local>
2018-04-20 11:03:10 +0200

    Converted Comment to interface

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -2,11 +2,11 @@ import * as firebase from 'firebase';
 import DocumentReference = firebase.firestore.DocumentReference;
 import FieldValue = firebase.firestore.FieldValue;
 
-export class Comment {
-  public title: string;
-  public category: string;
-  public description: string;
-  public patient: DocumentReference;
+export interface Comment {
+  title: string;
+  category: string;
+  description: string;
+  patient: DocumentReference;
   // public imageUrls: string[];
-  public createdAt: FieldValue;
+  createdAt: FieldValue;
 }
