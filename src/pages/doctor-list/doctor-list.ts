168b05224ec251c26bd78e42f8089e75b8b82b55
Ashish Husain <ashish.m.husain@gmail.com>
2018-04-24 15:32:23 -0800

    Added the popover functionality in the doctor-list

--- src/pages/doctor-list/doctor-list.ts
+++ src/pages/doctor-list/doctor-list.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
+import { LoginPage } from '../../pages/login/login';
+import { LoginProvider } from '../../providers/login/login';
+import { PopoverComponent } from '../../components/popover/popover';
 
 @IonicPage()
 @Component({
@@ -8,5 +11,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DoctorListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public popoverCtrl: PopoverController, public loginProvider: LoginProvider) {}
+
+  public presentPopover(myEvent) {
+    const popover = this.popoverCtrl.create(PopoverComponent);
+    popover.present({
+      ev: myEvent,
+    });
+
+    popover.onDidDismiss((popoverData) => {
+      //alert(JSON.stringify(popoverData.item));
+      this.loginProvider.logout();
+      this.navCtrl.setRoot(LoginPage);
+    });
+  }
 }
